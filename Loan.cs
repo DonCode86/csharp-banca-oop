4abb9fc5f35370df9139a707e010246efe6c6c90
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-21 16:14:05 +0200

    add-constructor-loan

--- Loan.cs
+++ Loan.cs
@@ -7,4 +7,14 @@
 
     public DateTime? startDate { get; set; }
     public DateTime? endDate { get; set; }
+
+    public Loan (Customer customer, int loanAmount, int loanInstalament, DateTime? startDate, DateTime? endDate)
+    {
+        this.id = id;
+        this.customer = customer;
+        this.loanAmount = loanAmount;
+        this.loanInstalament = loanInstalament;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 }
